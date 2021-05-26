af5bf761f5027e17235cfc64d50c9f3ae128dfce
xuanthu5893 <83709248+xuanthu5893@users.noreply.github.com>
2021-05-26 17:25:40 +0700

    fix

--- Bai_Tap_04/src/Draw.java
+++ Bai_Tap_04/src/Draw.java
@@ -27,16 +27,16 @@ public class Draw {
         Scanner scanner = new Scanner(System.in);
         int n,m;
         do {
-            System.out.print("\nNhập chiều cao của hình chữ nhật n (n>0): ");
+            System.out.print("Nhập chiều cao của hình chữ nhật n (n>0): ");
             n = scanner.nextInt();
             if (n <= 0) System.out.print("Hãy nhập số > 0");
         } while (n <= 0);
         do {
-            System.out.print("\nNhập chiều ngang của hình chữ nhật m (m>0): ");
+            System.out.print("Nhập chiều ngang của hình chữ nhật m (m>0): ");
             m = scanner.nextInt();
             if (m <= 0) System.out.print("Hãy nhập số > 0");
         } while (m <= 0);
-
+        System.out.println("\nHình chữ nhật có ");
         System.out.println("Chiều cao = " + n);
         System.out.println("Chiều ngang = " + m);
         for (int i =1; i<=n; i++){
