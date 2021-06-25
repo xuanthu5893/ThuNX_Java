548bee6f104bc076df0838883769985de4d4f17f
xuanthu5893 <83709248+xuanthu5893@users.noreply.github.com>
2021-06-25 18:06:10 +0700

    Update Work.java

--- Collection_Exercises/src/Work.java
+++ Collection_Exercises/src/Work.java
@@ -17,23 +17,23 @@ public class Work {
     public List nationalityList(List<Person> person) {
         List<String> list = new ArrayList<String>();
         for (Person p : person) {
-            if (!list.contains(p.nationality)) list.add(p.nationality);
+            if (!list.contains(p.nationality)) list.add(p.nationality); //nếu list chưa chứa quốc gia thì thêm quốc gia đó vào list
         }
         return list;
     }
 
     //Sắp xếp list theo tên người
     public List sortByName(List<Person> person) {
-        ArrayList<Person> list = new ArrayList<>();
+        List<Person> list = new ArrayList<>();
         for (Person p : person) {
             int size = list.size();
-            if (list.size() == 0) list.add(p);
+            if (list.size() == 0) list.add(p);//List chưa có gì thì thêm luôn vào
             else for (int i = 0; i < size; i++) {
-                if (p.name.compareTo(list.get(i).name) < 0) {
+                if (p.name.compareTo(list.get(i).name) < 0) {//nếu name < hơn name ở vị trí i thì add person đó vào vị trí i trong list, break
                     list.add(i, p);
                     break;
                 } else if (i == size - 1) {
-                    list.add(p);
+                    list.add(p); //nếu duyệt đến cuối list thì thêm person vào cuối 
                 }
             }
         }
